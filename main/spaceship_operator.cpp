dd5bd5cc6e2b62ba73ddceda2f88fc825bfcc8db
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-07-30 14:09:14 -0400

    Add files via upload

---
+++ main/spaceship_operator.cpp
@@ -0,0 +1,34 @@
+/*
+	The Spaceship operator is offered in the C++20 standard
+	A.k.a the three way operator. 
+
+	Have to use -std=c++20 with g++ command
+*/
+
+#include <iostream>
+#include <compare>  // Needed for spaceship operator
+
+using namespace std;
+
+int main()
+{
+	int a = 1, b = 2;
+
+	auto ans = a <=> b;  // Spaceship operator here
+
+	/*
+		Spaceship operator takes two parameters, a and b.
+			-If a < b, the result is:    ans < 0
+			-If a > b, the result is:    ans > 0
+			-If a == b, the results is:  ans == 0
+	*/
+
+	if (ans < 0)
+		cout << "a < b" << endl;
+	else if (ans > 0)
+		cout << "a > b" << endl;
+	else if (ans == 0)
+		cout << "a == b" << endl;
+
+	return 0;
+}
