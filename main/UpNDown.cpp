b9a78d38cb4e091cda52be36a39cdb2cbd90d851
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-08-02 11:15:57 -0400

    Update UpNDown.cpp

--- main/UpNDown.cpp
+++ main/UpNDown.cpp
@@ -15,14 +15,14 @@ int main()
 			* When no '=' in condition, target cannot be > than itself or < itself
 
 
-						        -
-					   	   -    #
-				      -    #    #     
+					    -
+				       -    #
+				  -    #    #     
 			     -    #    #    #		5 is not less than 5 (5 < 5)
 			-    #    #    #    #         
 			#    #    #    #    #
 	 		-    -    -    -    -
-		   < 5  < 5  < 5  < 5  == 5
+		       < 5  < 5  < 5  < 5  == 5
 	*/
 	for (int i = 1; i < 5; ++i)
 		cout << i << endl;
@@ -36,7 +36,7 @@ int main()
 		   #    #    #    #    -
 		   #    #    #    #    #  
 		   -    -    -    -    -
-		  > 1  > 1  > 1   > 1  == 1
+		  > 1  > 1  > 1  > 1  == 1
 	*/
 	for (int j = 5; j > 1; --j)
 		cout << j << endl;
