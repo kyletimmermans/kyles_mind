c236962fae92e9655ca3f3f5591d2b934fbab89d
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-08-17 16:23:06 -0400

    Add files via upload

---
+++ main/AndOr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	bool var1 = true;
+	bool var2 = true;
+	bool var3 = true;
+	bool var4 = true;
+	bool var5 = false;
+
+
+	// && operator needs everything to be true, this will not run
+	if (var1 && var2 && var3 && var4 && var5) {
+    	cout << "&&" << endl;
+	}
+
+
+	// || operator needs only one thing to be true, this will run
+	if (var1 || var2 || var3 || var4 || var5) {
+		cout << "||" << endl;
+	}
+
+
+	return 0;
+}
