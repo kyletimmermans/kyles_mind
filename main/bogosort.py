11788ec865b6029d32638030a19435ff6f313b51
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-08-26 01:18:09 -0400

    Add files via upload

---
+++ main/bogosort.py
@@ -0,0 +1,30 @@
+'''
+Bogosort - Generate random numbers until it's sorted
+
+Worst case: Infinity
+'''
+
+import random
+
+myList = [1, 9, 8, 7, 6, 4, 5, 3, 2, 0, 10]
+
+sortedList = sorted(myList)
+
+'''
+-While the list is not equal to it's sorted state
+ then it's unsorted
+
+-If it's not equal to it's sorted self, it's not sorted
+
+*Take what it's supposed to be, if it's not
+ what it's supposed to be, keep going
+'''
+while (sortedList != myList):
+	myList = [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10),
+			  random.randint(0, 10), random.randint(0, 10), random.randint(0, 10),
+			  random.randint(0, 10), random.randint(0, 10), random.randint(0, 10),
+			  random.randint(0, 10)]
+	print(myList)
+
+# On success
+print(myList)
