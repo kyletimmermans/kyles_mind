c4cc5707b6897fbe2cf8b437cbf8801e4ff7bbc0
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-09-06 12:01:11 -0400

    Update removeDuplicates.py

--- main/removeDuplicates.py
+++ main/removeDuplicates.py
@@ -5,6 +5,10 @@ The newest and first elements to go into newList
 cannot be dupes because they have not been seen yet.
 Once the element is in newList and it is found in both newList
 and oldList, it means that it has already been added, thus making it a duplicate.
+
+* If it's in newList already, we'll be adding a copy,
+  a duplicate. If it's not in newList, we haven't seen
+  it and it is not a copy, it is completely new and unique.
 '''
 
 oldList = ["hello", "cool", "cool", "awesome"]
