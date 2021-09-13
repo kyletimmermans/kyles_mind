7d6bfbd2e9c22d304ba3a7a230c0e2a3d4904418
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-09-12 20:32:03 -0400

    Add files via upload

---
+++ main/isleapyear.c
@@ -0,0 +1,30 @@
+#include <stdio.h> // Header files
+#include <stdlib.h>
+
+// main func w/ args
+int main(int argc, char **argv) 
+{
+
+   // If no args or more than one arg (argv[0] - program name, argv[1] - year)
+   if (argc != 2) {
+      printf("Too few / too many args given!");
+      exit(1); // Exit with error
+   }
+
+   // atoi() converts char array to int
+   int year = atoi(argv[1]);
+
+   // Is divisible by 4 && not divisible by 100 = leap year
+   // Is divisible by 400 = leap year
+   // Everything else = not leap year
+   if ((year % 4 == 0) && (year % 100 != 0)) { // Fits both criteria
+      printf("%s was a leap year\n", argv[1]);  
+   } else if (year % 400 == 0) {  // Fits one criteria but gets second chance
+      printf("%s was a leap year\n", argv[1]);
+   } else {  // Fits no criteria
+      printf("%s was not a leap year\n", argv[1]);
+   }
+
+   // Terminate program successfully
+   return 0;
+}
