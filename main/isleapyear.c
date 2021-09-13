9d1c695c5627a94e44ec0ba0d99a571348ef48ff
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-09-12 21:09:06 -0400

    Update isleapyear.c

--- main/isleapyear.c
+++ main/isleapyear.c
@@ -14,9 +14,11 @@ int main(int argc, char **argv)
    // atoi() converts char array to int
    int year = atoi(argv[1]);
 
-   // Is divisible by 4 && not divisible by 100 = leap year
-   // Is divisible by 400 = leap year
-   // Everything else = not leap year
+   /* 
+      Is divisible by 4 && not divisible by 100 = leap year
+      Is divisible by 400 = leap year
+      Everything else = not leap year
+   */
    if ((year % 4 == 0) && (year % 100 != 0)) { // Fits both criteria
       printf("%s was a leap year\n", argv[1]);  
    } else if (year % 400 == 0) {  // Fits one criteria but gets second chance
@@ -25,9 +27,31 @@ int main(int argc, char **argv)
       printf("%s was not a leap year\n", argv[1]);
    }
    
-   // *E.g. Blue and yellow makes green
-   //       Also just green makes green
-
+   
+   /*
+        Explanation of if-statement flow:
+        
+        1. If condition1 && condition2 are both true, then leap year
+        2. If either condition1 && condition2 is false,
+           we check the next if-statement if condition3 is true
+        
+        
+        2 trues:
+        condition1==true && condition2==true --> leap year
+        
+        1 true, 1 false:
+        condition1==true && condition==false
+        |
+        |
+         ---> condition3==true --> leap year
+         
+        
+        * condition 1 and 2 or just condition 3
+         
+        * E.g. Blue and yellow makes green
+          Also just green makes green
+   */
+   
    // Terminate program successfully
    return 0;
 }
