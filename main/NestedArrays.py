4c8d584c37e4cebf3401aa59b810312d14c8f6c6
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2022-02-12 15:26:56 -0500

    Update NestedArrays.py

--- main/NestedArrays.py
+++ main/NestedArrays.py
@@ -5,6 +5,8 @@ e.g. The first choice (index 0) is 3
 In nested arrays, an index becomes a value and that value becomes and index which
 in turn gets us another value.
 
+The inner array will act as the index for the next outer array.
+
 Index --> Value --> Index --> Value
 
 We evaluate from right to left by simplifying the inner-most array
