10fb0383f93d2f1cc9f7bded1beb1990aed465e9
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2022-03-22 18:18:33 -0400

    Add files via upload

---
+++ main/NestedForLoopLogic.py
@@ -0,0 +1,53 @@
+'''
+Nested For Loop Logic:
+
+
+A nested loop is a loop within a loop, an inner loop within the body of an outer one. 
+How this works is that the first pass of the outer loop triggers the inner loop, which 
+executes to completion. Then the second pass of the outer loop triggers the inner loop 
+again. This repeats until the outer loop finishes.
+	
+	-https://tldp.org/LDP/abs/html/nestedloops.html
+
+E.g.
+
+1st:								2nd:
+---									---
+							
+\									 ^
+ \ Outer loop triggers 			 	  \ Once the inner-most loop finishes,
+  \  inner loops, until we			   \  we move up to the outer-loops
+   \   get to the inner-most loop       \  and finish the loops one by one 
+    \								     \   until we get to the outer-most loop 
+     V                                    \    and finish it last
+'''
+
+
+for i in range(2):
+	print("For loop i")
+	
+	for j in range(2):
+		print("	For loop j")
+		
+		for k in range(2):
+			print("		For loop k")
+
+
+'''
+Output:
+
+For loop i
+	For loop j
+		For loop k
+		For loop k
+	For loop j
+		For loop k
+		For loop k
+For loop i
+	For loop j
+		For loop k
+		For loop k
+	For loop j
+		For loop k
+		For loop k
+'''
