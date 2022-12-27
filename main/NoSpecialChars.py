3d4d5243fc11f26379531b857c2d3fa629d1ef58
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2022-12-26 19:41:22 -0500

    Add files via upload

---
+++ main/NoSpecialChars.py
@@ -0,0 +1,34 @@
+username = "user1"
+
+
+# It is False if there are no True's in any()
+#    --------------------------------------------
+#    |								  			^
+#    v								  			|
+if any(not c.isalnum() for c in username) == False:
+
+
+
+# Where True indicates c.isalnum() being False
+#	    |						           |	
+#	    |	    ----------------------------
+#	    |       |
+#	    v	    v    
+if any(not c.isalnum() for c in username) == False:
+
+
+
+# c.isalnum() being False means there is a special char in the username
+#	  |				   				     		  |			
+#	  -----------		  	-----------------------
+#				|		   |
+#           	v   	   v
+if any(not c.isalnum() for c in username) == False:
+
+
+'''
+Therefore, if there are no True's in any() "== False", then there were
+no isalnum() False's turned into True's by the not keyword,
+and since there were no False's from isalnum(), then there were no
+special characters.
+'''
