f1a63654b6df6a5e116a50365271f7199f9b41fa
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2022-12-26 19:51:43 -0500

    Update NoSpecialChars.py

--- main/NoSpecialChars.py
+++ main/NoSpecialChars.py
@@ -2,27 +2,27 @@ username = "user1"
 
 
 # It is False if there are no True's in any()
-#    --------------------------------------------
-#    |								  			                  ^
-#    v								  			                  |
+#ㅤㅤ--------------------------------------------
+#ㅤㅤ|ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ^
+#ㅤㅤvㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ|
 if any(not c.isalnum() for c in username) == False:
 
 
 
 # Where True indicates c.isalnum() being False
-#	    |						                         |	
-#	    |	        ----------------------------
-#	    |         |
-#	    v	        v    
+#ㅤㅤㅤㅤㅤ|ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ|	
+#ㅤㅤㅤㅤㅤ|ㅤㅤㅤㅤㅤ----------------------------
+#ㅤㅤㅤㅤㅤ|ㅤㅤㅤㅤㅤ|
+#ㅤㅤㅤㅤㅤvㅤㅤㅤㅤㅤv    
 if any(not c.isalnum() for c in username) == False:
 
 
 
 # c.isalnum() being False means there is a special char in the username
-#	  |				   				     		                    |			
-#	  -----------		  	     ------------------------
-#				      |		         |
-#           	v   	       v
+#ㅤㅤㅤㅤ|ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ|			
+#ㅤㅤㅤㅤ-----------ㅤㅤㅤㅤㅤㅤㅤ------------------------
+#ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ|ㅤㅤㅤㅤㅤㅤㅤ|
+#ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤvㅤㅤㅤㅤㅤㅤㅤv
 if any(not c.isalnum() for c in username) == False:
 
 
@@ -31,4 +31,14 @@ Therefore, if there are no True's in any() "== False", then there were
 no isalnum() False's turned into True's by the not keyword,
 and since there were no False's from isalnum(), then there were no
 special characters.
+
+So if we get all False's in the any() e.g. any(False, False, False) then
+all of the c.isalnum() were True and became False of the 'not' keyword and if
+isalnum() was True for every character, then all of the characters were
+alphanumeric and not special characters.
+
+And if we even get one True in the any() e.g. any(False, True, False). then
+one of the c.isalnum() was False and became True because of the 'not' keyword
+and if isalnum() was False, then one of the characters in the username was
+not alphanumeric and therefore a special character.
 '''
