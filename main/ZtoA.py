1c3e061a6fc74bcae4624372961e14729eb5e282
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2023-09-06 17:57:53 -0400

    Add files via upload

--- main/ZtoA.py
+++ main/ZtoA.py
@@ -50,12 +50,12 @@ For example: cipher 'k' translates to normal 'p'.
 
 
 
-            1st: ord(char) - ord('a')
-         -------------------------------
+            1st: ord(char) - ord('a')                         2nd: ord('z') - ()
+         -------------------------------                -------------------------------
 Plain:  [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, [p], q, r, s, t, u, v, w, x, y, z]
-                                                       ^
-                2nd: ord('z') - ()                     |
-         -------------------------------               |
+                                                       
+                2nd: ord('z') - ()                         1st: ord(char) - ord('a')
+         -------------------------------                -------------------------------                  
 Cipher: [z, y, x, w, v, u, t, s, r, q, p, o, n, m, l, [k], j, i, h, g, f, e, d, c, b, a]
                                                       
 '''
